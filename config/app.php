2d3c1cc8abcb7e08cb150958daaf5bdb215dc403
Alex_P <pekler-sasha@yandex.ru>
2016-03-25 10:18:14 +0500

    Adding IDE Laravel helper use "artisan ide-helper:generate" to generate ide helper file

--- config/app.php
+++ config/app.php
@@ -155,6 +155,11 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        /*
+         * IDE Laravel helper
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
