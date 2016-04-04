9500f264d404e8a5213a48aff299f66087299f4f
Alex_P <pekler-sasha@yandex.ru>
2016-04-04 18:11:17 +0500

    Adding some seed (test)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        $Sevents = ['myevent' => 2, 'someevent' => 2, 'omgevent' => 1, 'pizdes' => 3];
+        $Sevents = ['myevent' => 2, 'someevent' => 2, 'omgevent' => 1, 'pizdes' => 3, 'test' => 3];
         $Seventt = [1 => 'puck', 2 => 'hh', 3 => 'mocha'];
 
         foreach ($Seventt as $id => $name) DB::table('events_types')->insert(['id' => $id, 'name' => $name]);
