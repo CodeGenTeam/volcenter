0ee3bcd9ce90ca028fe41b332caf419e84a86a6a
ko22009 <ko22009@yandex.ru>
2016-04-07 23:26:10 +0500

    remove uneless files

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,56 +1,5 @@
 <?php
 
 Route::get('/', function () {
-    return view('index');
-    //Auth::login($user);
-});
-
-Route::get('/events', function () {
-    return view('events');
-});
-
-Route::get('/event/{id}', function ($id) {
-    return View::make('event', [
-        'id' => $id
-    ]);
-});
-
-// only admin
-Route::get('/eventcreate', function () {
-    return view('eventcreate');
-});
-
-// only auth users
-Route::get('/settings', function () {
-    return view('settings');
-});
-
-//
-Route::get('/exit', function () {
-    Auth::logout();
-    return redirect('/');
-});
-
-//user profile
-Route::get('/user/{id}', function($id) { ////'Auth\AuthController@postRegister');
-    return View::make('user', [
-        'id' => $id
-    ]);
-});
-
-Route::get('/list', 'EventController@index');
-Route::get('/list{page}', 'EventController@page')->where(['page' => '\d+']);
-
-Route::group(['middleware' => 'web'], function () {
-
-    Route::auth();
-
-    Route::group(['middleware' => 'auth'], function () {
-
-        Route::get('/home', function () {
-            return view('home');
-        });
-
-
-    });
+    return view('welcome');
 });
--- public/js/app.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('app', ['ui.bootstrap'])
-
-.constant('info', {
-	ver: '0.02'
-})
