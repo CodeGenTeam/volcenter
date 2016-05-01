d58b6eba381e835681dd6cb5f5e7dd86b6fc4a8c
Alex_P <pekler-sasha@yandex.ru>
2016-05-01 21:32:52 +0500

    Adding short method for return util

--- app/Http/ReturnUtil.php
+++ app/Http/ReturnUtil.php
@@ -6,6 +6,10 @@ use Response;
 
 class ReturnUtil {
 
+    public function ret($assert, $data = null, $dataOnFail = false) {
+        return $assert ? $this->success($data) : $this->fail($dataOnFail ?? $data);
+    }
+
     public function success($data = null) {
         return Response::json(array_merge(['success' => true], $data ? [] : is_array($data) ? $data : ['note' => $data]));
     }
