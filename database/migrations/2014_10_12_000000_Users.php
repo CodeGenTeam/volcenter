10720dc848929f02f747a2a2b3b33d891a4e0606
ko22009 <ko22009@yandex.ru>
2016-05-24 20:57:48 +0500

    change last,first,middle name in migration Users

--- database/migrations/2014_10_12_000000_Users.php
+++ database/migrations/2014_10_12_000000_Users.php
@@ -17,9 +17,9 @@ class Users extends Migration
             $table->increments('id');
             $table->string('login')->uneque();
             $table->string('email')->unique();
-            $table->string('name1'); // Фамилия
-            $table->string('name3'); // Имя
-            $table->string('name2'); // Отчество
+            $table->string('firstname'); // Имя
+            $table->string('lastname'); // Фамилия
+            $table->string('middlename'); // Отчество
             $table->timestamp('birthday')->default(DB::raw('0')); // др
             $table->string('password');
             $table->rememberToken();
