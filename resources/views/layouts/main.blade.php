9309df81cc75e9f328efce0d75078cf878c14987
ko22009 <ko22009@yandex.ru>
2016-05-26 18:40:23 +0500

    bootstrap js

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,6 +15,8 @@
     <link rel="stylesheet" href="http://cdn.webix.com/edge/webix.css" type="text/css">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
     <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     <script src="http://cdn.webix.com/edge/webix.js" type="text/javascript"></script>
     <script src="https://cdn.jsdelivr.net/vue/latest/vue.js"></script>
 </head>
