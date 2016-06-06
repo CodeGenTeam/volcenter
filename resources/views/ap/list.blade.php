fddc4c09772a882e973323d6e1ed324eb4433749
pekler-sasha@ya.ru <alex199712665>
2016-06-06 19:00:56 +0500

    Adding material bootstrap

--- resources/views/ap/list.blade.php
+++ resources/views/ap/list.blade.php
@@ -1,6 +1,14 @@
 <html>
     <head>
         <title>{{ 'Админ-панель' . ( is_array($modules) ? '' : $modules->getName()) }}</title>
+        <link rel="stylesheet" type="text/css" href="/ap-bin/css/bootstrap.css"/>
+        <link rel="stylesheet" type="text/css" href="/ap-bin/css/material.css"/>
+        <link rel="stylesheet" type="text/css" href="/ap-bin/css/material-fullpalette.css"/> {{-- это тестовая палитра нужна ток ради подкрашивания элементов --}}
+
+        <script type="text/javascript" src="/ap-bin/js/bootstrap.js"></script>
+        <script type="text/javascript" src="/ap-bin/js/jQuery.js"></script>
+        <script type="text/javascript" src="/ap-bin/js/material.js"></script>
+        <script type="text/javascript" src="/ap-bin/js/ripples.js"></script>
     </head>
     <body>
         <div class="container">
