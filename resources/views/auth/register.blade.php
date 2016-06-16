027b982814492fae6ebce1501946539656d48bb4
AlexP9712 <pekler-sasah@ya.ru>
2016-06-16 16:41:28 +0500

    Adding registration prototype

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,82 +1,90 @@
 @extends('layouts.main')
 
+<?php
+$fields = [
+        [
+                'id' => 'login',
+                'descr' => 'Логин',
+        ],
+        [
+                'id' => 'email',
+                'descr' => 'Почта',
+                'type' => 'email',
+        ],
+        [
+                'id' => 'firstname',
+                'descr' => 'Имя',
+        ],
+        [
+                'id' => 'lastname',
+                'descr' => 'Фамилия',
+        ],
+        [
+                'id' => 'middlename',
+                'descr' => 'Отчество',
+        ],
+        [
+                'replace' => 'birthday.field',
+        ],
+        [
+                'id' => 'password',
+                'descr' => 'Пароль',
+                'type' => 'password',
+        ],
+        [
+                'id' => 'password_confirmation',
+                'descr' => 'Повторите пароль',
+                'type' => 'password',
+        ],
+];
+?>
+
 @section('content')
-	<div class="container">
-		<div class="row">
-			<div class="col-md-8 col-md-offset-2">
-				<div class="panel panel-default">
-					<div class="panel-heading">Register</div>
-					<div class="panel-body">
-						<form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
-							{!! csrf_field() !!}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Регистрация</div>
+                    <div class="panel-body">
+                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+                            {!! csrf_field() !!}
 
-							<div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-								<label class="col-md-4 control-label">Name</label>
+                            @section('birthday.field')
+                                {{-- TODO запилить выбор даты рождения --}}
+                            @endsection
 
-								<div class="col-md-6">
-									<input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                            @foreach ($fields as $field)
+                                @if (!isset($field['replace']))
+                                    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                                        <label class="col-md-4 control-label">{{ $field['descr'] }}</label>
+                                        <div class="col-md-6">
+                                            <input type="{{ $field['type'] ?? 'text' }}" class="form-control"
+                                                   name="{{ $field['id'] }}"
+                                                   value="{{ old('name') }}">
 
-									@if ($errors->has('name'))
-										<span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-									@endif
-								</div>
-							</div>
+                                            @if ($errors->has($field['id']))
+                                                <span class="help-block">
+                                                <b>{{ $errors->first($field['id']) }}</b>
+                                            </span>
+                                            @endif
+                                        </div>
+                                    </div>
+                                @else
+                                    @yield($field['replace'])
+                                @endif
+                            @endforeach
 
-							<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-								<label class="col-md-4 control-label">E-Mail Address</label>
-
-								<div class="col-md-6">
-									<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-									@if ($errors->has('email'))
-										<span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-									@endif
-								</div>
-							</div>
-
-							<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-								<label class="col-md-4 control-label">Password</label>
-
-								<div class="col-md-6">
-									<input type="password" class="form-control" name="password">
-
-									@if ($errors->has('password'))
-										<span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-									@endif
-								</div>
-							</div>
-
-							<div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-								<label class="col-md-4 control-label">Confirm Password</label>
-
-								<div class="col-md-6">
-									<input type="password" class="form-control" name="password_confirmation">
-
-									@if ($errors->has('password_confirmation'))
-										<span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-									@endif
-								</div>
-							</div>
-
-							<div class="form-group">
-								<div class="col-md-6 col-md-offset-4">
-									<button type="submit" class="btn btn-primary">
-										<i class="fa fa-btn fa-user"></i>Register
-									</button>
-								</div>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        <i class="fa fa-btn fa-user"></i>Register
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
