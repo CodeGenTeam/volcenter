82bd1e48e3de82b3ce33333df47dcca39f3c8303
ko22009 <ko22009@yandex.ru>
2016-06-19 20:26:08 +0500

    default route free

--- app/AdminPanel/AdminPanelMiddleware.php
+++ app/AdminPanel/AdminPanelMiddleware.php
@@ -11,6 +11,8 @@ class AdminPanelMiddleware {
     public function handle(Request $req, Closure $next) {
         //if (env('APP_DEBUG')) return $next($req);
         if (!Auth::check()) return redirect('/');
+        $path = $req->decodedPath();
+        if (!preg_match('/^adminpanel\/?.*/', $path)) return $next($req); // если это не админпанель
         $role = Auth::user()->role();
         if ($role!='admin' && $role!='moderator') return abort(403);
         return $next($req);
