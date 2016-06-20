6cab259fea2d669c95d9bd0d3ac3813cd18c6f23
ko22009 <ko22009@yandex.ru>
2016-06-20 23:58:19 +0500

    fix middleware

--- app/AdminPanel/AdminPanelMiddleware.php
+++ app/AdminPanel/AdminPanelMiddleware.php
@@ -10,9 +10,9 @@ class AdminPanelMiddleware {
 
     public function handle(Request $req, Closure $next) {
         //if (env('APP_DEBUG')) return $next($req);
-        if (!Auth::check()) return redirect('/');
         $path = $req->decodedPath();
         if (!preg_match('/^adminpanel\/?.*/', $path)) return $next($req); // если это не админпанель
+        if (!Auth::check()) return redirect('/');
         $role = Auth::user()->role();
         if ($role!='admin' && $role!='moderator') return abort(403);
         return $next($req);
